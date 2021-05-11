d444cfb91086e59a5be23b18d13037234e664e2a
Shachin <shachinchakma85@gmail.com>
2021-05-11 12:38:23 +0600

    fully completed initial ui

---
+++ assets/js/custom-script/student-account/student-attendance-script.js
@@ -0,0 +1,102 @@
+const classesData = [{
+        id: 1,
+        dep: 'complete',
+        session: '2018-2019',
+        semester: '1',
+        course: 'CSE-121',
+        date: '01-04-2021',
+        isActive: true
+    },
+    {
+        id: 2,
+        dep: 'complete',
+        session: '2018-2019',
+        semester: '3',
+        course: 'CSE-333',
+        date: '01-04-2021',
+        isActive: true
+    },
+    {
+        id: 3,
+        dep: 'complete',
+        session: '2018-2019',
+        semester: '3',
+        course: 'CSE-333',
+        date: '03-04-2021',
+        isActive: true
+    },
+    {
+        id: 4,
+        dep: 'incomplete',
+        session: '2018-2019',
+        semester: '3',
+        course: 'EEE-353',
+        date: '01-04-2021',
+        isActive: true
+    },
+    {
+        id: 5,
+        dep: 'EEE',
+        session: '2018-2019',
+        semester: '3',
+        course: 'EEE-333',
+        date: '01-04-2021',
+        isActive: false
+    },
+    {
+        id: 6,
+        dep: 'CSE',
+        session: '2018-2019',
+        semester: '5',
+        course: 'CSE-353',
+        date: '03-04-2021',
+        isActive: false
+    },
+];
+
+var searchField = document.getElementById('selectDep');
+var boxesDiv = document.getElementById('boxes');
+
+console.log(boxesDiv)
+
+const displayClasses = (data) => {
+    data.map(classData => {
+        const {
+            session,
+            semester,
+            course,
+            isActive,
+            id
+        } = classData;
+
+        let createDiv = document.createElement('div');
+        createDiv.classList.add('box');
+
+        createDiv.innerHTML = `
+            <div class="${isActive ? 'class-active' : 'class-unactive'}"></div>
+            <a data-page="02.student-attendance-page-2.html" class="myBtn${id}" onmouseenter="helper(${id})">
+                <p>SESSION: ${session}</p>
+                <p>SEMESTER: ${semester}</p>
+                <p>COURSE: ${course}</p>
+            </a>
+        `
+        boxesDiv.appendChild(createDiv);
+        console.log('i am being called');
+    });
+};
+
+//load data
+displayClasses(classesData);
+
+const filterDataAndDisplay = (data, searchInput) => {
+    const filteredData = data.filter(classData =>
+        classData.dep.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    boxesDiv.innerHTML = " "; //clearing previous state
+    displayClasses(filteredData);
+}
+
+//gets called everytime input changes
+searchField.addEventListener('change', (event) => {
+    filterDataAndDisplay(classesData, event.target.value);
+})
